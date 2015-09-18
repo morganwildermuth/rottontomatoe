b7d138ec22b1e78129603ac610f5636c3550a65c
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-09-17 20:58:59 -0700

    Initial Commit

---
+++ rottonTomatoe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rottonTomatoe
+//
+//  Created by Morgan Wildermuth on 9/17/15.
+//  Copyright © 2015 WEF6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
