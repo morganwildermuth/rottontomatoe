dbe948877d5f52bd8b3202e7c9f4cbbff3757779
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-09-19 22:07:48 -0700

    view detail shows name of film

--- rottonTomatoe/FilmsViewDetailController.swift
+++ rottonTomatoe/FilmsViewDetailController.swift
@@ -12,10 +12,33 @@ class FilmsViewDetailController: UIViewController {
     var selectedFilm: NSDictionary?
 
     @IBOutlet weak var filmPoster: UIImageView!
+    @IBOutlet weak var filmTitle: UILabel!
+    @IBOutlet weak var filmDetailsView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         if let film = selectedFilm {
-            puts("\(selectedFilm!)")
+
+            filmTitle.text = film["title"] as! String
+
+            var filmPosterUrl = (film["posters"] as! NSDictionary)["thumbnail"] as! String
+            let range = filmPosterUrl.rangeOfString(".*cloudfront.net/", options: .RegularExpressionSearch)
+            if let range = range {
+                filmPosterUrl = filmPosterUrl.stringByReplacingCharactersInRange(range, withString: "https://content6.flixster.com/")
+            }
+            let image_url = NSURL(string: filmPosterUrl)
+            let url_request = NSURLRequest(URL: image_url!)
+            let placeholder = UIImage(named: "no_photo")
+            filmPoster.setImageWithURLRequest(url_request, placeholderImage: placeholder, success: { [weak filmPoster] (request:NSURLRequest!,response:NSHTTPURLResponse!, image:UIImage!) -> Void in
+                if let retrievedPoster = image {
+                    filmPoster!.image = retrievedPoster
+                }
+                }, failure: { [weak filmPoster]
+                    (request:NSURLRequest!,response:NSHTTPURLResponse!, error:NSError!) -> Void in
+                    filmPoster!.image = nil
+                })
+
+
         }
 
         // Do any additional setup after loading the view.
