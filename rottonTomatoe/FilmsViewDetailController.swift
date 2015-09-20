e7abfcf21be16d91d3dfd9aea9a3924d6e48614a
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-09-20 02:05:40 -0700

    details view has animation for its tab

--- rottonTomatoe/FilmsViewDetailController.swift
+++ rottonTomatoe/FilmsViewDetailController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FilmsViewDetailController: UIViewController {
     var selectedFilm: NSDictionary?
+    var filmDetailsViewActive = true
     
     @IBOutlet weak var filmPoster: UIImageView!
     @IBOutlet weak var filmTitle: UILabel!
@@ -42,9 +43,36 @@ class FilmsViewDetailController: UIViewController {
                     filmPoster!.image = nil
                 })
         }
+        moveFilmDetailsView()
         // Do any additional setup after loading the view.
     }
+    
 
+    private func toggleFilmDetailsView(){
+        filmDetailsViewActive = !filmDetailsViewActive
+        moveFilmDetailsView()
+    }
+    
+    private func moveFilmDetailsView(){
+        if filmDetailsViewActive{
+            UIView.animateWithDuration(1, animations:  {
+            self.filmDetailsView.frame = CGRect(x: 0, y: 650, width: self.filmDetailsView.frame.width, height: self.filmDetailsView.frame.height)
+            })
+        } else {
+            UIView.animateWithDuration(1, animations:  {
+                self.filmDetailsView.frame = CGRect(x: 0, y: 370, width: self.filmDetailsView.frame.width, height: self.filmDetailsView.frame.height)
+            })
+        }
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        puts("toggle!")
+        toggleFilmDetailsView()
+    }
+    
+    @IBAction func onSwipe(sender: AnyObject) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
